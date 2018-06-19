eb04f9a7ab3ed80a2cbb19cc04151cae79e6887b
Pieter <acexof_clubs@yahoo.com>
2018-06-19 16:07:24 -0400

    Final Version of Disk Drop
    There were some errors that were not working, but it's good now.

---
+++ Disk Drop/Assets/Scripts/BounceNoise.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BounceNoise : MonoBehaviour {
+	//private Animator animator;
+
+	//void Start()
+	//{
+		//animator = GetComponent<Animator> ();
+	//}
+
+	void OnCollisionEnter2D(Collision2D hit)
+	{
+		if (hit.gameObject.tag == "Pin")
+		{
+			GetComponent<AudioSource> ().Play ();
+			//animator.SetTrigger ("playerHit");
+
+		}
+	}
+
+}
